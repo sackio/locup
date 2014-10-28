fa88553c46f93ebd21c832cb8c34b65cc0df768f
Ben Sack <ben@sack.io>
2014-10-28 13:02:28 -0400

    added mapquest

--- test/locup_test.js
+++ test/locup_test.js
@@ -9,26 +9,6 @@ var Belt = require('jsbelt')
   , Locup = new require('../lib/locup.js')(_.extend({'api_key': O.google.server_api_key}, O))
 ;
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 exports['units'] = {
   setUp: function(done) {
     // setup here
@@ -68,16 +48,16 @@ exports['units'] = {
         return cb();
       }
     , function(cb){
-        return Locup.get_coordinates(globals.address1, Belt.cs(cb, globals, 'coords', 1, 0));
+        return Locup.providers.google.get_coordinates(globals.address1, Belt.cs(cb, globals, 'coords', 1, 0));
       }
     , function(cb){
-        return Locup.get_formatted_address(globals.address1, Belt.cs(cb, globals, 'addr', 1, 0));
+        return Locup.providers.google.get_formatted_address(globals.address1, Belt.cs(cb, globals, 'addr', 1, 0));
       }
     , function(cb){
-        return Locup.get_components(globals.address1, 'street_number', Belt.cs(cb, globals, 'streetnum', 1, 0));
+        return Locup.providers.google.get_components(globals.address1, 'street_number', Belt.cs(cb, globals, 'streetnum', 1, 0));
       }
     , function(cb){
-        return Locup.get_components(globals.address3, 'country', Belt.cs(cb, globals, 'country', 1, 0));
+        return Locup.providers.google.get_components(globals.address3, 'country', Belt.cs(cb, globals, 'country', 1, 0));
       }
     , function(cb){
         test.ok(globals.addr);
@@ -90,21 +70,51 @@ exports['units'] = {
         return cb();
       }
     , function(cb){
-        globals.geo1 = [40.689167, -74.044444];
-        return Locup.reverse_geocode(globals.geo1[0], globals.geo1[1], Belt.cs(cb, globals, 'rev1', 1, 0));
+        globals.geo5 = [40.689167, -74.044444];
+        return Locup.providers.google.reverse_geocode(globals.geo5[0], globals.geo5[1], Belt.cs(cb, globals, 'rev1', 1, 0));
       }
     /*, function(cb){
         test.ok(_.some(globals.rev1, function(r){ return r.formatted_address === '1 Liberty Island, Liberty Island, New York, NY 10004, USA'; }));
         return cb();
       }*/
     , function(cb){
-        return Locup.get_address_components(globals.geo1[0], globals.geo1[1]
+        return Locup.providers.google.get_address_components(globals.geo5[0], globals.geo5[1]
                , 'locality', Belt.cs(cb, globals, 'locality', 1, 0));
       }
     , function(cb){
         test.ok(globals.locality[0] === 'New York');
         return cb();
       }
+    , function(cb){
+        return Locup.providers.mapquest.geocode(globals.address1, Belt.cs(cb, globals, 'geo1b', 1, 0));
+      }
+    , function(cb){
+        return Locup.providers.mapquest.geocode(globals.address2, Belt.cs(cb, globals, 'geo2b', 1, 0));
+      }
+    , function(cb){
+        return Locup.providers.mapquest.geocode(globals.address3, Belt.cs(cb, globals, 'geo3b', 1, 0));
+      }
+    , function(cb){
+        return Locup.providers.mapquest.reverse_geocode(globals.geo5[0], globals.geo5[1], Belt.cs(cb, globals, 'geo5b', 1, 0));
+      }
+    /*, function(cb){
+        return Locup.providers.mapquest.geocode(globals.address4, function(err, res){
+          test.ok(err);
+          return cb();
+        });
+      }*/
+    , function(cb){
+        test.ok(globals.geo1b);
+        test.ok(globals.geo2b);
+        test.ok(globals.geo3b);
+        test.ok(globals.geo5b);
+
+        return cb();
+      }
+    /*, function(cb){
+        console.log(JSON.stringify(globals.geo5b, null, 2));
+        return cb();
+      }*/
     ], function(err){
       if (err) console.error(err);
       test.ok(!err);
